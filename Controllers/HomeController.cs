be9163be5457d504de94c00343ff4a6c74d1af72
Amnashakel786 <amnashakeel187@gmail.com>
2025-01-03 11:15:05 +0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // About page
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        // Service page
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        // Contact page
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
+
+    }
+}
